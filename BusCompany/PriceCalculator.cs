2fc1b16baa715ef553f264667502f8211204ae08
Henrik Kühl <henrikkyhl@hotmail.com>
2018-09-13 00:58:27 +0200

    Iteration 2

--- BusCompany/PriceCalculator.cs
+++ BusCompany/PriceCalculator.cs
@@ -5,9 +5,10 @@ namespace BusCompany
     public class PriceCalculator
     {
         int initialFee = 2500;
+        int kmFee = 10;
 
         public decimal CalculatePrice(int distance){
-            decimal price = initialFee;
+            decimal price = initialFee + kmFee*distance;
             return price;
 
         }
--- Buscompany.UnitTests/PriceCalculatorTests.cs
+++ Buscompany.UnitTests/PriceCalculatorTests.cs
@@ -5,17 +5,29 @@ namespace BusCompany.UnitTests
 {
     public class PriceCalculatorTests
     {
+        PriceCalculator calculator;
+
+        public PriceCalculatorTests(){
+            calculator = new PriceCalculator();
+        }
+
         [Fact]
         public void CalculatePrice_DistanceIs0_PriceShouldBe2500()
         {
-            //Arrange
-            PriceCalculator calculator = new PriceCalculator();
-
             //Act
             decimal price = calculator.CalculatePrice(0);
-
             //Assert
             Assert.Equal(2500, price);
         }
+
+        [Fact]
+        public void CalculatePrice_DistanceIs1_PriceShouldBe2510()
+        {
+            //Act
+            decimal price = calculator.CalculatePrice(1);
+            //Assert
+            Assert.Equal(2510, price);
+        }
+
     }
 }
